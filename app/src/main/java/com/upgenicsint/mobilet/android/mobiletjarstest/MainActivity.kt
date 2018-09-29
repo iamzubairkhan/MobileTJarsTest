5f47fd12d03777cb0b492c06687bd7c551bc0970
ZubairKhan <mzubair.ciit@gmail.com>
2018-09-29 17:41:09 +0500

    Working on checkForPermission

--- app/src/main/java/com/upgenicsint/mobilet/android/mobiletjarstest/MainActivity.kt
+++ app/src/main/java/com/upgenicsint/mobilet/android/mobiletjarstest/MainActivity.kt
@@ -19,8 +19,9 @@ class MainActivity : AppCompatActivity() {
     var PERMISSIONS = arrayOf(android.Manifest.permission.ACCESS_WIFI_STATE,
             android.Manifest.permission.CHANGE_WIFI_STATE,
             android.Manifest.permission.ACCESS_FINE_LOCATION,
-            android.Manifest.permission.ACCESS_COARSE_LOCATION,
-            android.Manifest.permission.CAMERA)
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION)
 
     lateinit var scanResult: MutableList<ScanResult>
     lateinit var wifiManager:WifiManager
@@ -32,15 +33,14 @@ class MainActivity : AppCompatActivity() {
 
         val enableButton = findViewById<Button>(R.id.button1)
         val disableButton = findViewById<Button>(R.id.button2)
-        val wifiManager:WifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
         enableButton.setOnClickListener {
-            wifiManager.isWifiEnabled = true
+            TurnWIFI(true)
             scanNearbySSID()
         }
 
         disableButton.setOnClickListener {
-            wifiManager.isWifiEnabled = false
+            TurnWIFI(false)
         }
 
         if (hasPermissions(this, PERMISSIONS))
@@ -69,7 +69,7 @@ class MainActivity : AppCompatActivity() {
             scanResult = wifiManager.scanResults
 
             for (result in scanResult){
-                Log.v("Results", "SSID: ${result.SSID} strenght ${result.level}")
+                Log.v("PermissionResults", "SSID: ${result.SSID} strengthRSSI: ${result.level} calculateSignalLevel ${WifiManager.calculateSignalLevel(result.level, 100)}")
             }
         }
     }
@@ -99,13 +99,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun isWIFIEnabled(): Boolean{
-        val wifi = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        return wifi.isWifiEnabled
+    private fun isWIFIEnabled(): Boolean{
+        return wifiManager.isWifiEnabled
     }
 
-    fun TurnWIFI(OnOrOff: Boolean){
-        val wifi = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        wifi.isWifiEnabled = OnOrOff
+    private fun TurnWIFI(OnOrOff: Boolean){
+        wifiManager.isWifiEnabled = OnOrOff
     }
 }
--- app/src/main/java/com/upgenicsint/mobilet/android/mobiletjarstest/WiFiManager.kt
+++
@@ -1,9 +0,0 @@
-package com.upgenicsint.mobilet.android.mobiletjarstest
-
-import com.upgenicsint.mobilet.android.mobiletcore.Permissions
-
-class WiFiManager : Permissions {
-    override fun CheckForPermission() {
-
-    }
-}
